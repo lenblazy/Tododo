042ebd4f7133bee4548ecd2643c1ba2f65579c60
lenblazy <lenblazy@gmail.com>
2023-06-17 17:15:59 +0300

    Initial Commit

---
+++ Tododo/TododoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TododoApp.swift
+//  Tododo
+//
+//  Created by Lennox Mwabonje on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TododoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
